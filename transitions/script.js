534a67c022d72b61a7d5c01c066c2f2f934b375f
Praful Chandra <chandra.s.praful@gmail.com>
2025-02-14 15:15:19 +0530

    animating discrete behaviours css

--- transitions/script.js
+++ transitions/script.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM Loaded");
-  const element = document.querySelector(".square");
-  const circle = document.querySelector(".circle");
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("DOM Loaded");
+//   const element = document.querySelector(".square");
+//   const circle = document.querySelector(".circle");
 
-  document.addEventListener("click", (e) => {
-    element.style.transform = `translateY(${
-      e.clientY - element.clientHeight / 2
-    }px) translateX(${e.clientX - element.clientWidth / 2}px)`;
-  });
+//   document.addEventListener("click", (e) => {
+//     element.style.transform = `translateY(${
+//       e.clientY - element.clientHeight / 2
+//     }px) translateX(${e.clientX - element.clientWidth / 2}px)`;
+//   });
 
-  //   document.addEventListener("mousemove", (e) => {
-  //     circle.style.transform = `translateY(${
-  //       e.clientY - circle.clientHeight / 2
-  //     }px) translateX(${e.clientX - circle.clientWidth / 2}px)`;
-  //   });
+//   //   document.addEventListener("mousemove", (e) => {
+//   //     circle.style.transform = `translateY(${
+//   //       e.clientY - circle.clientHeight / 2
+//   //     }px) translateX(${e.clientX - circle.clientWidth / 2}px)`;
+//   //   });
 
-  element.addEventListener("transitionrun", (e) => {
-    console.log(e);
-  });
-  element.addEventListener("transitionstart", (e) => {
-    console.log(e);
-  });
-  element.addEventListener("transitionend", (e) => {
-    if (e.propertyName == "transform") {
-      var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      element.style.backgroundColor = `#${randomColor}`;
-    }
-  });
-});
+//   element.addEventListener("transitionrun", (e) => {
+//     console.log(e);
+//   });
+//   element.addEventListener("transitionstart", (e) => {
+//     console.log(e);
+//   });
+//   element.addEventListener("transitionend", (e) => {
+//     if (e.propertyName == "transform") {
+//       var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//       element.style.backgroundColor = `#${randomColor}`;
+//     }
+//   });
+// });
